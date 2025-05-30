c16917d7257d5cdbb4e4b7cd3f0282fe6e68c8b0
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-29 23:01:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_livraria
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_livraria/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_livraria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
